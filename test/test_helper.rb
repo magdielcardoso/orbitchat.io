b4ac931772a2a02e5cc393320938aaffbcee93d6
Magdiel Cardoso <magdielsilva.contato@gmail.com>
2025-06-01 17:58:04 -0300

    feat: Adicionando database_cleaner do test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,20 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "database_cleaner/active_record"
 
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
+    DatabaseCleaner.strategy = :transaction
+
+    setup { DatabaseCleaner.start }
+    teardown { DatabaseCleaner.clean }
+
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+    # fixtures :all
 
     # Add more helper methods to be used by all tests here...
   end
