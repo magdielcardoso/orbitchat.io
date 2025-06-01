08d983f0663d7af9270265837efa63147d2a0fa1
Magdiel Cardoso <magdielsilva.contato@gmail.com>
2025-06-01 18:05:02 -0300

    test: Melhorando test de acount

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,5 @@
 class Account < ApplicationRecord
+  has_many :users, dependent: :destroy
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -2,10 +2,7 @@ require "test_helper"
 
 class AccountTest < ActiveSupport::TestCase
   def setup
-    @account = Account.new(
-      name: "Empresa Exemplo",
-      email: "contato@exemplo.com"
-    )
+    @account = Account.create!(name: "Empresa Exemplo", email: "contato@exemplo.com")
   end
 
   test "deve ser válido com atributos obrigatórios" do
@@ -33,5 +30,32 @@ class AccountTest < ActiveSupport::TestCase
     assert_not @account.valid?
     @account.email = "valido@email.com"
     assert @account.valid?
+  end
+
+  test "deve permitir atualizar nome e email para valores válidos" do
+    @account.update!(name: "Nova Empresa", email: "novo@email.com")
+    assert_equal "Nova Empresa", @account.reload.name
+    assert_equal "novo@email.com", @account.reload.email
+  end
+
+  test "não deve permitir atualizar email para um já existente" do
+    outra = Account.create!(name: "Outra", email: "outra@email.com")
+    assert_raises ActiveRecord::RecordInvalid do
+      outra.update!(email: @account.email)
+    end
+  end
+
+  test "deve permitir destruir account sem dependências obrigatórias" do
+    account = Account.create!(name: "Temp", email: "temp@email.com")
+    assert_difference("Account.count", -1) do
+      account.destroy
+    end
+  end
+
+  test "ao destruir account, usuários associados devem ser destruídos se dependente" do
+    user = User.create!(email: "user_#{SecureRandom.hex(4)}@exemplo.com", password: "senha123", password_confirmation: "senha123", account: @account)
+    assert_difference("User.count", -1) do
+      @account.destroy
+    end
   end
 end
