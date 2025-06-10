ff5d7ad3f08319600235a2b29e839de50a6c0fbd
Magdiel Cardoso <dzmartins.contato@icloud.com>
2025-06-10 14:53:35 -0300

    refactor: Remove InertiaExample controller and Vue component as part of cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   resources :inboxes
   resources :messages
   resources :users
+
   devise_for :users
 
-  get "inertia-example", to: "inertia_example#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
